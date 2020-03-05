3c30fd8ce6b32bc35038f5bcd19378e1ca6b81fb
Thalumas <mixit@blueyonder.co.uk>
2020-03-05 11:02:48 +0000

    Added initial Rocket control script.

---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Process_Input();
+    }
+
+    private void Process_Input() {
+
+        if (Input.GetKey(KeyCode.Space)) {
+            print("Space Pressed!");
+        }
+
+        if (Input.GetKey(KeyCode.A) & !Input.GetKey(KeyCode.D)) {
+            print("A Pressed!");
+        }
+
+        if (Input.GetKey(KeyCode.D) & !Input.GetKey(KeyCode.A)) {
+            print("D Pressed!");
+        }
+
+    }
+}
