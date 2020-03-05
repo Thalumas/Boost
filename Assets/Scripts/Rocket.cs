2664c3aa5c628b2fa7a72053079367dca6c6b2cf
Thalumas <mixit@blueyonder.co.uk>
2020-03-05 13:03:37 +0000

    Added rocket thrust sound.

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -5,10 +5,21 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    public int thrustPower = 12;
+    public int rotationSpeed = 90;
+    private Transform initialTransform;
+    private Vector3 thrustVector;
+
+    Rigidbody rigidBody;
+    AudioSource engineSound;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        initialTransform = transform;
+        rigidBody = GetComponent<Rigidbody>();
+        engineSound = GetComponent<AudioSource>();
+        thrustVector = Vector3.up * thrustPower;
     }
 
     // Update is called once per frame
@@ -19,16 +30,26 @@ public class Rocket : MonoBehaviour
     private void Process_Input() {
 
         if (Input.GetKey(KeyCode.Space)) {
-            print("Space Pressed!");
+            rigidBody.AddRelativeForce(thrustVector);
+            if (!engineSound.isPlaying) { 
+                engineSound.Play();
+            }
+        } else {
+            engineSound.Stop();
         }
 
         if (Input.GetKey(KeyCode.A) & !Input.GetKey(KeyCode.D)) {
-            print("A Pressed!");
+            transform.Rotate(Vector3.forward, rotationSpeed * Time.deltaTime);
         }
 
         if (Input.GetKey(KeyCode.D) & !Input.GetKey(KeyCode.A)) {
-            print("D Pressed!");
+            transform.Rotate(-Vector3.forward, rotationSpeed * Time.deltaTime);
         }
 
+        if (Input.GetKey(KeyCode.R)) {
+            print("Resetting.....");
+            transform.localRotation = initialTransform.rotation;
+            transform.localPosition = initialTransform.position;
+        }
     }
 }
