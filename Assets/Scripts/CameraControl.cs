fcef8e400672b37e90dc511d89a89c9ca0ad6b02
Thalumas <mixit@blueyonder.co.uk>
2020-03-06 18:11:44 +0000

    Added tracking camera.

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    [SerializeField] Rocket rocket;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(rocket.transform.position.x,
+                                         rocket.transform.position.y,
+                                         -20);
+    }
+}
